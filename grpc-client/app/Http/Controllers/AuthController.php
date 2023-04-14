fe2bb085e0a14a2c547c0ec24bb1d56c935f7518
Gregorius Ongo <gregoriusongo@gmail.com>
2023-04-14 17:11:38 +0700

    handle response

--- grpc-client/app/Http/Controllers/AuthController.php
+++ grpc-client/app/Http/Controllers/AuthController.php
@@ -4,26 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use LoginUserRequest;
+use RegisterUserRequest;
 
 class AuthController extends Controller
 {
-    public function register(){
-        return response()->json(["status" => "ok"]);
+    protected $client;
+
+    public function __construct()
+    {
+        // create grpc client
+        $this->client = new \AuthServiceClient('grpc-server-node:50051', ['credentials' => \Grpc\ChannelCredentials::createInsecure()]);
+    }
+
+    public function register(Request $request){
+        
+        $registerRequest = new RegisterUserRequest();
+        $registerRequest->setUsername($request->input('username'));
+        $registerRequest->setPassword($request->input('password'));
+    
+        list($registerResponse, $status) = $this->client->RegisterUser($registerRequest)->wait();
+        // dd($status);
+        return response()->json([
+            'status' => $status->code,
+            'message' => $registerResponse->getMessage(),
+        ]);
     }
 
     public function login(Request $request){
-        $client = new \AuthServiceClient('grpc-server-node:50051', ['credentials' => \Grpc\ChannelCredentials::createInsecure()]);
+        
         $loginRequest = new LoginUserRequest();
         $loginRequest->setUsername($request->input('username'));
         $loginRequest->setPassword($request->input('password'));
     
-        list($loginResponse, $status) = $client->LoginUser($loginRequest)->wait();
-        dd($status);
-        // return response()->json([
-        //     'status' => $status->code,
-        //     'token' => $loginResponse->getToken(),
-        // ]);
-
-        return response()->json(["status" => "ok"]);
+        list($loginResponse, $status) = $this->client->LoginUser($loginRequest)->wait();
+        // dd($status);
+        return response()->json([
+            'status' => $status->code,
+            'token' => $loginResponse->getToken(),
+        ]);
     }
 }
