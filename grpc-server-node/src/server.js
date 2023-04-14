ab404638edb7c8c957cb69f2e80c0edd87138e49
Gregorius Ongo <gregoriusongo@gmail.com>
2023-04-14 17:11:18 +0700

    add db and function code

--- grpc-server-node/src/server.js
+++ grpc-server-node/src/server.js
@@ -1,7 +1,8 @@
 var PROTO_PATH = __dirname + '/../protos/auth.proto';
 var grpc = require('@grpc/grpc-js');
 var protoLoader = require('@grpc/proto-loader');
-// Suggested options for similarity to existing grpc.load behavior
+const sqlite3 = require('sqlite3').verbose();
+
 var packageDefinition = protoLoader.loadSync(
    PROTO_PATH,
    {
@@ -12,26 +13,60 @@ var packageDefinition = protoLoader.loadSync(
       oneofs: true
    });
 var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-// The protoDescriptor object has the full package hierarchy
+
 var authService = protoDescriptor.AuthService;
 
-var auth = require('./auth');
+const db = new sqlite3.Database(':memory:');
+// db migration
+db.serialize(() => {
+   db.run('CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)');
+});
 
 /**
- * Starts an RPC server that receives requests for the Greeter service at the
- * sample server port
+ * Starts an RPC server
  */
 function main() {
    var server = new grpc.Server();
-   server.addService(authService.service, { LoginUser: login });
+   server.addService(authService.service, {
+      LoginUser: login,
+      RegisterUser: register,
+   });
    server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
       console.log("server started at 0.0.0.0:50051");
       server.start();
    });
 }
 
-function login(){
-   console.log('OK');
+/**
+ * Register new user to database
+ */
+function register(call, callback) {
+   const { username, password } = call.request;
+
+   db.run('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], (error) => {
+      if (error) {
+         callback({ message: error.message }, null);
+      } else {
+         callback(null, { success: true });
+      }
+   });
+}
+
+/**
+ * Login function, get user from database
+ */
+function login(call, callback) {
+   const { username, password } = call.request;
+
+   db.get('SELECT * FROM users WHERE username = ?', [username], (error, row) => {
+      if (error) {
+         callback({ message: error.message }, null);
+      } else if (!row || row.password !== password) {
+         callback(null, { success: false, message: 'Incorrect username or password' });
+      } else {
+         callback(null, { success: true });
+      }
+   });
 }
 
 main();
